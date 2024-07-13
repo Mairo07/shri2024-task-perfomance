62e31c913c31fc163d14bf616469f309a5a6a6c8
Daria Anashkina <mairou@yandex.ru>
2024-07-14 01:10:45 +0300

    remove setTimeout

--- src/index.jsx
+++ src/index.jsx
@@ -354,12 +354,11 @@ function Main() {
   </main>;
 }
 
-setTimeout(() => {
-  const root = ReactDOM.createRoot(document.getElementById('app'));
-  root.render(
-      <>
-          <Header />
-          <Main />
-      </>
-  );
-}, 100);
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+    <>
+        <Header />
+        <Main />
+    </>
+);
